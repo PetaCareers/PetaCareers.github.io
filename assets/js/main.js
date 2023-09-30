9ea413bbdda324f5f760c16ecdd4e9707265e77d
PetaCareers <petacareers.id@gmail.com>
2023-09-30 20:21:18 +0700

    Complete the Beta vers

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,14 +36,45 @@ const shadowHeader = () =>{
 window.addEventListener('scroll',shadowHeader)
 /*=============== EMAIL JS ===============*/
 
-
+contactForm.addEventListener('submit', sendEmail)
 /*=============== SHOW SCROLL UP ===============*/ 
-
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                        : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const SECTIONS = document.querySelectorAll('selection[id]')
 
+const scrollActive = () =>{
+    const scrollDown = window.scrollY
+
+    SECTIONS.forEach(CURRENT =>{
+        const selectionHeigth = CURRENT.offsetHeight,
+            sectionTop = CURRENT.offsetTop - 58,
+            sectionId = CURRENT.getAttribute('id'),
+            sectionsClass = document.querySelector('.nav__menua[href*=' + sectionId + ']')
+        if(scrollDown > screenTop && scrollDown <= sectionTop + sectionHeigth){
+        sectionsClass.classList.add('active-link')
+        }else{
+        sectionsClass.classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== DARK LIGHT THEME ===============*/ 
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+})
+
+sr.reveal('.home__peta, .about__image', {origin: 'right'})
+sr.reveal('.home__name, .home__info, .about__container', {origin: 'left'})
